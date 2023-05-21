ae2b7a071ce95903fdb0e663632bbc3cc0de9bd9
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-05-21 11:16:39 +0300

    feat: index layout

---
+++ client/send.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://gbtitan@developer@95.140.156.17:15672', function(error0, connection) {
+
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        var queue = 'hello';
+        var msg = 'Hello World!';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+        channel.sendToQueue(queue, Buffer.from(msg));
+
+        console.log(" [x] Sent %s", msg);
+    });
+    setTimeout(function() {
+        connection.close();
+        process.exit(0);
+    }, 1500);
+});
