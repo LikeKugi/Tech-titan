0e621d6c5d9c44d3a3ed9f34a3950045c47fb9ba
Petr Sumkin <sumkin.gpb@gmail.com>
2023-05-29 00:21:43 +0300

    Update InternCareer.jsx

--- tech-titans/src/components/roles/Intern/InternCareer/InternCareer.jsx
+++ tech-titans/src/components/roles/Intern/InternCareer/InternCareer.jsx
@@ -6,7 +6,7 @@ const InternCareer = () => {
           <ul>
             <li><Link to={'https://talent.mos.ru/'} target={'_blank'}>Карьерный портал правительства Москвы</Link> </li>
             <li><Link to={'https://mguu.ru/'} target={'_blank'}>Университет Правительства Москвы</Link> </li>
-            <li><Link to={'https://t.me/ugskpm'} target={'_blank'}>Teleram</Link> </li>
+            <li><Link to={'https://t.me/ugskpm'} target={'_blank'}>Telegram</Link> </li>
           </ul>
       </section>
   );
