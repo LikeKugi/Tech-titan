b8b00f8e4115761945cc6a41e06b78d65d6c56a2
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-05-28 07:30:19 +0300

    fix: fix layout to center

--- tech-titans/src/components/screen/Energies/Energies.jsx
+++ tech-titans/src/components/screen/Energies/Energies.jsx
@@ -1,8 +1,11 @@
 import './energies.css'
-import {useEffect, useState} from "react";
+import {useEffect, useReducer, useState} from "react";
+
 const Energies = (props) => {
   const {min, max} = props;
   const [energiesLvl, setEnergiesLvl] = useState(min);
+
+  const [show, toggle] = useReducer(show => !show, false);
 
   useEffect(() => {
     const ht = Math.floor(Math.random() * (max - min) + min);
@@ -10,13 +13,19 @@ const Energies = (props) => {
   }, [min, max])
 
   return (
-      <aside className='energies'>
-        <p>Энергия</p>
-        <div className='energies__wrapper'>
-            <div className='energies__level' style={{height: energiesLvl + '%',}}></div>
-        </div>
-        <p>Уровень: {energiesLvl}%</p>
-      </aside>
+      <>
+      <div className="energy">
+        <button className="energy__toggle" onClick={toggle}>Уровень энергии</button>
+      </div>
+        {show && <aside className='energies'>
+          <p>Энергия</p>
+          <div className='energies__wrapper'>
+            <div className='energies__level'
+                 style={{height: energiesLvl + '%',}}></div>
+          </div>
+          <p>Уровень: {energiesLvl}%</p>
+        </aside>}
+      </>
   );
 }
 export default Energies;
