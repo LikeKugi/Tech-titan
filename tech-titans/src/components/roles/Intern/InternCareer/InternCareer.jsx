4f4cf228d22b28bff2a9889c7875b783a98196ef
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-05-28 15:56:16 +0300

    fix: change link on intern page

--- tech-titans/src/components/roles/Intern/InternCareer/InternCareer.jsx
+++ tech-titans/src/components/roles/Intern/InternCareer/InternCareer.jsx
@@ -4,9 +4,9 @@ const InternCareer = () => {
   return (
       <section>
           <ul>
-            <li><Link to={'https://t.me/ugskpm'} target={'_blank'}>Карьерный портал правительства Москвы</Link> </li>
+            <li><Link to={'https://talent.mos.ru/'} target={'_blank'}>Карьерный портал правительства Москвы</Link> </li>
             <li><Link to={'https://mguu.ru/'} target={'_blank'}>Университет Правительства Москвы</Link> </li>
-            <li><Link to={'https://talent.mos.ru/'} target={'_blank'}>Teleram</Link> </li>
+            <li><Link to={'https://t.me/ugskpm'} target={'_blank'}>Teleram</Link> </li>
           </ul>
       </section>
   );
